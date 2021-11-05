3094b78bbce88f9df5b71e8de8ac2c5ce737374a
Florian Zwagemaker <florian@niqex.com>
2021-11-05 16:47:16 +0100

    fix: change consensus calling to a 'mid' cpu-task

--- ViroConstrictor/runconfigs.py
+++ ViroConstrictor/runconfigs.py
@@ -60,7 +60,7 @@ def SnakemakeParams(conf, cores, ref, prim, feats, platform, samplesheet, amplic
             "QC": threads_midcpu,
             "AdapterRemoval": threads_lowcpu,
             "PrimerRemoval": threads_highcpu,
-            "Consensus": threads_lowcpu,
+            "Consensus": threads_midcpu,
             "Index": threads_lowcpu,
             "Typing": threads_lowcpu,
         },
