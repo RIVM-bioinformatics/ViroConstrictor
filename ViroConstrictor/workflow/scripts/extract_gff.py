43b41ebfaca22ebf435f2c2cb442afb2c08f9ec1
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-03-29 12:59:03 +0200

    refactor: add basic gff filter script

---
+++ ViroConstrictor/workflow/scripts/extract_gff.py
@@ -0,0 +1,12 @@
+import gffpandas.gffpandas as gffpd
+import sys
+
+_, input, output, refID = sys.argv
+
+gff = gffpd.read_gff3(input)
+
+header = gff.header
+
+gff.df = gff.df[gff.df.seq_id == refID]
+
+gff.to_gff3(output)
