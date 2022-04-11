a1e7093ff58a1b077131d5b898a7d4e7b85e38e5
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-11 14:31:12 +0200

    docs: Add explanatory docstrings to all functions in `ViroConstrictor.samplesheet`

--- ViroConstrictor/samplesheet.py
+++ ViroConstrictor/samplesheet.py
@@ -10,6 +10,20 @@ import yaml
 
 
 def illumina_sheet(inputdir):
+    '''Function takes a directory as input, and returns a dictionary of dictionaries, where the keys of the outer
+    dictionary are the sample names, and the keys of the inner dictionaries are the read numbers (R1 and
+    R2)
+    
+    Parameters
+    ----------
+    inputdir
+        The directory where the fastq files are located.
+    
+    Returns
+    -------
+        A dictionary of dictionaries.
+    
+    '''
     illuminapattern = re.compile(r"(.*)(_|\.)R?(1|2)(?:_.*\.|\..*\.|\.)f(ast)?q(\.gz)?")
     samples = {}
     for dirname, subdir, filename in os.walk(inputdir):
@@ -22,6 +36,19 @@ def illumina_sheet(inputdir):
 
 
 def nanopore_sheet(inputdir):
+    '''Function takes a directory as input, and returns a dictionary of sample names and their corresponding file
+    paths
+    
+    Parameters
+    ----------
+    inputdir
+        the directory where the fastq files are located
+    
+    Returns
+    -------
+        A dictionary with the sample name as the key and the full path to the file as the value.
+    
+    '''
     nanoporepattern = re.compile(r"(.*)\.f(ast)?q(\.gz)?")
     samples = {}
     for dirname, subdir, filename in os.walk(inputdir):
@@ -33,6 +60,19 @@ def nanopore_sheet(inputdir):
 
 
 def iontorrent_sheet(inputdir):
+    '''Function takes a directory as input, and returns a dictionary of sample names and their corresponding file
+    paths
+    
+    Parameters
+    ----------
+    inputdir
+        the directory where the fastq files are located
+    
+    Returns
+    -------
+        A dictionary with the sample name as the key and the full path to the file as the value.
+    
+    '''
     iontorrentpattern = re.compile(r"(.*)\.f(ast)?q(\.gz)?")
     samples = {}
     for dirname, subdir, filename in os.walk(inputdir):
@@ -44,6 +84,20 @@ def iontorrent_sheet(inputdir):
 
 
 def GetSamples(inputdir, platform):
+    '''Wrapping function taking in a directory and sequencing platform, triggers appropriate sub-function and returns a dictionary of samples
+    
+    Parameters
+    ----------
+    inputdir
+        the directory where the sample sheets are located
+    platform
+        the sequencing platform used to generate the data.
+    
+    Returns
+    -------
+        A dict of samples
+    
+    '''
     if platform == "illumina":
         samples = illumina_sheet(inputdir)
     if platform == "nanopore":
