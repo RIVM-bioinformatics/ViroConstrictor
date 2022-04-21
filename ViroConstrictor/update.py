acfbf1b057ef0444e0ed1a8bf7a87bc1d913ed11
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-21 13:35:42 +0200

    refactor: rework self updater

--- ViroConstrictor/update.py
+++ ViroConstrictor/update.py
@@ -13,17 +13,9 @@ from .userprofile import AskPrompts
 
 def update(sysargs, conf):
 
-    autocontinue = False
-    ask_prompt = False
-
-    if conf["GENERAL"]["auto_update"] == "yes":
-        autocontinue = True
-
-    if autocontinue is False:
-        if conf["GENERAL"]["ask_for_update"] == "yes":
-            ask_prompt = True
-
-    if autocontinue is True:
+    autocontinue = conf["GENERAL"]["auto_update"] == "yes"
+    ask_prompt = not autocontinue and conf["GENERAL"]["ask_for_update"] == "yes"
+    if autocontinue:
         try:
             latest_release = request.urlopen(
                 "https://api.github.com/repos/RIVM-bioinformatics/Viroconstrictor/releases"
@@ -107,79 +99,63 @@ If you won't turn off the auto-updater we'll keep nagging you about this until y
             sys.exit(1)
         return
 
-    if autocontinue is False and ask_prompt is False:
+    if not ask_prompt:
         return
 
-    if autocontinue is False and ask_prompt is True:
-        try:
-            latest_release = request.urlopen(
-                "https://api.github.com/repos/RIVM-bioinformatics/Viroconstrictor/releases"
-            )
-        except Exception as e:
-            sys.stderr.write("Unable to connect to GitHub API\n" f"{e}")
-            return
-
-        latest_release = json.loads(latest_release.read().decode("utf-8"))[0]
-
-        latest_release_tag = latest_release["tag_name"]
-        latest_release_tag_tidied = LooseVersion(
-            latest_release["tag_name"].lstrip("v").strip()
+    try:
+        latest_release = request.urlopen(
+            "https://api.github.com/repos/RIVM-bioinformatics/Viroconstrictor/releases"
         )
+    except Exception as e:
+        sys.stderr.write("Unable to connect to GitHub API\n" f"{e}")
+        return
 
-        localversion = LooseVersion(__version__)
+    latest_release = json.loads(latest_release.read().decode("utf-8"))[0]
 
-        if (
+    latest_release_tag = latest_release["tag_name"]
+    latest_release_tag_tidied = LooseVersion(
+        latest_release["tag_name"].lstrip("v").strip()
+    )
+
+    localversion = LooseVersion(__version__)
+
+    if (
             localversion < latest_release_tag_tidied
             and localversion.version[0] == latest_release_tag_tidied.version[0]
         ):
-            if (
-                AskPrompts(
-                    f"""
+        if AskPrompts(f"""
 There's a new version of ViroConstrictor available.
 
 Current version: {color.RED + color.BOLD}{'v' + __version__}{color.END}
-Latest version: {color.GREEN + color.BOLD}{latest_release_tag}{color.END}\n""",
-                    f"""Do you want to update? [yes/no] """,
-                    ["yes", "no"],
-                    fixedchoices=True,
-                )
-                == "yes"
-            ):
-                subprocess.run(
-                    [
-                        sys.executable,
-                        "-m",
-                        "pip",
-                        "install",
-                        "--upgrade",
-                        f"git+https://github.com/RIVM-bioinformatics/ViroConstrictor@{latest_release_tag}",
-                    ],
-                    check=True,
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                )
-
-                print(
-                    f"ViroConstrictor updated to {color.YELLOW + color.BOLD}{latest_release_tag}{color.END}"
-                )
-
-                subprocess.run(sysargs)
-                sys.exit(0)
-            print(f"Skipping update to version {latest_release_tag}")
-            print(f"Continuing...")
-            return
-        if (
-            localversion < latest_release_tag_tidied
-            and localversion.version[0] != latest_release_tag_tidied.version[0]
-        ):
-            print(
-                f"{color.RED}There's a new version of ViroConstrictor available. This new version is a {color.UNDERLINE}major{color.END + color.RED} update and cannot be installed automatically.{color.END}"
+Latest version: {color.GREEN + color.BOLD}{latest_release_tag}{color.END}\n""", """Do you want to update? [yes/no] """, ["yes", "no"], fixedchoices=True) == "yes":
+            subprocess.run(
+                [
+                    sys.executable,
+                    "-m",
+                    "pip",
+                    "install",
+                    "--upgrade",
+                    f"git+https://github.com/RIVM-bioinformatics/ViroConstrictor@{latest_release_tag}",
+                ],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
             )
+
             print(
-                f"""
-Current version: {color.RED + color.BOLD}{'v' + __version__}{color.END}
-Latest version: {color.GREEN + color.BOLD}{latest_release_tag}{color.END}
-                """
+                f"ViroConstrictor updated to {color.YELLOW + color.BOLD}{latest_release_tag}{color.END}"
             )
-            print("Continuing without updating...\n")
+
+            subprocess.run(sysargs)
+            sys.exit(0)
+        print(f"Skipping update to version {latest_release_tag}")
+        print("Continuing...")
         return
+    if localversion < latest_release_tag_tidied:
+        print(
+            f"{color.RED}There's a new version of ViroConstrictor available. This new version is a {color.UNDERLINE}major{color.END + color.RED} update and cannot be installed automatically.{color.END}"
+        )
+        print(f"""Current version: {color.RED + color.BOLD}v{__version__}{color.END}\nLatest version: {color.GREEN + color.BOLD}{latest_release_tag}{color.END}""")
+
+        print("Continuing without updating...\n")
+    return
