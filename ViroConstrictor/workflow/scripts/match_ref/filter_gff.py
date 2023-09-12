45fe8a971f9c2c92720c1d1eccde4797d2e4dca5
Florian Zwagemaker <f.zwagemaker@gmail.com>
2023-09-12 16:43:39 +0200

    refactor: simplify the filter_gff.py script by using the AminoExtract functions

--- ViroConstrictor/workflow/scripts/match_ref/filter_gff.py
+++ ViroConstrictor/workflow/scripts/match_ref/filter_gff.py
@@ -2,49 +2,27 @@ import os
 import sys
 
 import pandas as pd
+import AminoExtract
 
 refdata, gfffile, outfile, updatedstats = sys.argv[1:]
 
-gffheader = ""
-with open(gfffile, "r") as f:
-    for line in f:
-        if line.startswith("#"):
-            gffheader += line
-        else:
-            break
-
-gffdf = pd.read_csv(
-    gfffile,
-    sep="\t",
-    comment="#",
-    names=[
-        "seqid",
-        "source",
-        "type",
-        "start",
-        "end",
-        "score",
-        "strand",
-        "phase",
-        "attributes",
-    ],
-)
+gff = AminoExtract.read_gff(gfffile, split_attributes=False)
 
 df = pd.read_csv(refdata, keep_default_na=False)
 
 seqrecordNames = df["seqrecord_name"].tolist()
-gffdf = gffdf.loc[gffdf["seqid"].isin(seqrecordNames)].reset_index(drop=True)
+gff.df = gff.df.loc[gff.df["seqid"].isin(seqrecordNames)].reset_index(drop=True)
 
 df["Feat_file"] = os.path.abspath(outfile)
 
 # replace the value of the "seqid" column in gffdf with the value of the "Reference" column in df, if the 'seqid' value in gffdf corresponds to the value of the 'seqrecord_name' column in df
-gffdf["seqid"] = gffdf["seqid"].map(df.set_index("seqrecord_name")["Reference"])
+gff.df["seqid"] = gff.df["seqid"].map(df.set_index("seqrecord_name")["Reference"])
 
-feats_to_write = gffdf.to_csv(sep="\t", index=False, header=False)
+feats_to_write = gff.df.to_csv(sep="\t", index=False, header=False)
 
 
 with open(outfile, "w") as f:
-    f.write(gffheader)
+    f.write(gff.header)
     f.write(feats_to_write)
 
 df.to_csv(updatedstats, index=False)
