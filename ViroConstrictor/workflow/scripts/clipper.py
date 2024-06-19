9ce0263bb00bcb822ac5e04b9e2afe805d52aaff
Florian Zwagemaker <f.zwagemaker@gmail.com>
2024-06-19 12:42:09 +0200

    fix: ensure proper spliced-section length filtering
    feat: add extra filtering options to clipper.py for future presets/usecases + adhoc alignment wrangling

--- ViroConstrictor/workflow/scripts/clipper.py
+++ ViroConstrictor/workflow/scripts/clipper.py
@@ -47,6 +47,24 @@ arg.add_argument(
 )
 
 arg.add_argument(
+    "--max-aligned-length",
+    metavar="FLOAT",
+    type=float,
+    required=False,
+    help="Filter reads based on the maximum length of the aligned part of the read",
+    default=0,
+)
+
+arg.add_argument(
+    "--only-include-region",
+    metavar="STRING",
+    type=str,
+    required=False,
+    help="Only include reads where the aligned section of the read start and end within the specified region",
+    default=None,
+)
+
+arg.add_argument(
     "--threads",
     metavar="Number",
     help="Number of threads that can be used for decompressing/compressing the BAM file",
@@ -59,6 +77,26 @@ flags = arg.parse_args()
 
 
 def split_cigar(cigar: str) -> list:
+    """
+    Split a CIGAR string into a list of tuples.
+
+    Parameters:
+    -----------
+    cigar : str
+        The CIGAR string to be split.
+
+    Returns:
+    --------
+    list
+        A list of tuples, where each tuple contains two elements:
+        - The number of consecutive characters.
+        - The character itself.
+
+    Example:
+    --------
+    >>> split_cigar("3M1I2D")
+    [(3, 'M'), (1, 'I'), (2, 'D')]
+    """
     cigar_tuples = []
     current_number = ""
     for char in cigar:
@@ -71,10 +109,42 @@ def split_cigar(cigar: str) -> list:
 
 
 def is_spliced(cigar_tuples: list) -> bool:
+    """
+    Check if the given list of cigar tuples contains any spliced regions.
+
+    Parameters
+    ----------
+    cigar_tuples : list
+        A list of cigar tuples representing the alignment.
+
+    Returns
+    -------
+    bool
+        True if there are spliced regions, False otherwise.
+    """
     return any(cigar_tuple[1] == "N" for cigar_tuple in cigar_tuples)
 
 
 def get_largest_spliced_len(cigar_tuples: list) -> int:
+    """
+    Calculate the largest spliced length from a list of cigar tuples.
+
+    Parameters:
+    -----------
+    cigar_tuples : list
+        A list of cigar tuples representing the cigar string.
+
+    Returns:
+    --------
+    int
+        The largest spliced length found in the cigar tuples.
+
+    Examples:
+    ---------
+    >>> cigar_tuples = [(10, 'M'), (5, 'N'), (20, 'M'), (15, 'N'), (30, 'M')]
+    >>> get_largest_spliced_len(cigar_tuples)
+    15
+    """
     largest_spliced_len = 0
     current_spliced_len = 0
     for cigar_tuple in cigar_tuples:
@@ -91,12 +161,27 @@ with flags.output as fileout:
     bamfile = pysam.AlignmentFile(flags.input, "rb", threads=flags.threads)
 
     reflength = bamfile.lengths[0]
-    minimal_read_length = int(reflength * flags.min_aligned_length)
+    minimal_read_length = int(flags.min_aligned_length)
+    maximum_read_length = int(
+        reflength if flags.max_aligned_length == 0 else flags.max_aligned_length
+    )
+
+    include_region_start = (
+        flags.only_include_region.split(":")[0]
+        if flags.only_include_region is not None
+        else None
+    )
+    include_region_end = (
+        flags.only_include_region.split(":")[1]
+        if flags.only_include_region is not None
+        else None
+    )
 
     for read in bamfile:
         read_start = read.query_alignment_start
         read_end = read.query_alignment_end
-
+        ref_start = read.reference_start
+        ref_end = read.reference_end
         trimmed_seq = read.query_alignment_sequence
         trimmed_qual = read.qual[read_start:read_end]
 
@@ -116,12 +201,22 @@ with flags.output as fileout:
             if is_spliced(cigartuples):
                 if (
                     get_largest_spliced_len(cigartuples)
-                    < flags.spliced_length_threshold
+                    > flags.spliced_length_threshold
                 ):
+
                     continue
 
         if read.query_alignment_length <= minimal_read_length:
             continue
+
+        if read.query_alignment_length >= maximum_read_length:
+            continue
+
+        if include_region_start is not None and include_region_end is not None:
+            if ref_start < int(include_region_start) or ref_end > int(
+                include_region_end
+            ):
+                continue
 
         fileout.write(
             "@"
