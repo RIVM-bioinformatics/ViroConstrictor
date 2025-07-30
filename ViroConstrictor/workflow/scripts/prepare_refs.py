864da4d1b1d495a5a7075e08bbb3bad2c9c80a0b
raaijmag <Gino.Raaijmakers@RIVM.nl>
2025-07-30 07:34:32 +0200

    refactor: re-wrote vcf_to_tsv and prepare refs in a modular way test: added unit test for vcf_to_tsv

--- ViroConstrictor/workflow/scripts/prepare_refs.py
+++ ViroConstrictor/workflow/scripts/prepare_refs.py
@@ -1,10 +1,44 @@
-import sys
+from argparse import ArgumentParser
+from pathlib import Path
 
 from Bio import SeqIO
 
-_, input, output, reference_id = sys.argv
+from ViroConstrictor.workflow.scripts.base_script_class import BaseScript
 
-for record in SeqIO.parse(str(input), "fasta"):
-    if reference_id in record.id:
-        record.seq = record.seq.upper()
-        SeqIO.write(record, str(output), "fasta")
+
+class PrepareRefs(BaseScript):
+    """
+    PrepareRefs script to process reference sequences.
+
+    This script reads a FASTA file, converts the sequence to uppercase,
+    and writes it to an output file if the reference ID matches.
+    """
+
+    def __init__(self, input_path: Path, output_path: Path, reference_id: str) -> None:
+        super().__init__(input_path, output_path)
+        self.reference_id = reference_id
+
+    @classmethod
+    def add_arguments(cls, parser: ArgumentParser) -> None:
+        super().add_arguments(parser)
+        parser.add_argument(
+            "--reference_id",
+            metavar="String",
+            help="Reference ID to match in the FASTA file.",
+            type=str,
+            required=True,
+        )
+
+    def run(self) -> None:
+        self._prepare_refs()
+
+    def _prepare_refs(self) -> None:
+        """Prepare reference sequences by converting to uppercase."""
+        for record in SeqIO.parse(self.input_path.as_posix(), "fasta"):
+            if self.reference_id in record.id:
+                record.seq = record.seq.upper()
+                SeqIO.write(record, self.output_path.as_posix(), "fasta")
+
+
+if __name__ == "__main__":
+    PrepareRefs.main()
